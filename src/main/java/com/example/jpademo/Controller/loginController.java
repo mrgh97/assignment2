9a85de83cc10aefad1b1b937c0b405d1d00e1ef9
mrgh97 <719475327@qq.com>
2019-04-16 12:56:51 +0800

    check

--- src/main/java/com/example/jpademo/Controller/loginController.java
+++ src/main/java/com/example/jpademo/Controller/loginController.java
@@ -1,4 +1,17 @@
 package com.example.jpademo.Controller;
 
+import com.example.jpademo.Service.memberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class loginController {
+    private memberService mService;
+
+    @Autowired
+    public void setmService(memberService m){
+        this.mService=m;
+    }
 }
